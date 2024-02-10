a607661f66e7f2a60317df5b501c3698953e7b39
PlusF <rkaneda896@gmail.com>
2024-02-10 11:24:36 +0900

    change concept style

--- pages/concept.tsx
+++ pages/concept.tsx
@@ -1,7 +1,33 @@
 import { SimpleBox } from '@/components/box';
 import { BackLinkBox } from '@/components/linkbox';
 import { NormalLogo } from '@/components/logo';
-import { Box, Heading, Image, Text } from '@chakra-ui/react';
+import {
+    Accordion,
+    AccordionButton,
+    AccordionIcon,
+    AccordionItem,
+    AccordionPanel,
+    Box,
+    Heading,
+    Image,
+    Text,
+} from '@chakra-ui/react';
+
+function ConceptItem(props: { title: string; children: string }) {
+    return (
+        <AccordionItem>
+            <Heading fontSize="md" mt="2">
+                <AccordionButton _expanded={{ bg: 'black', color: 'white' }}>
+                    <Box as="span" flex="1" textAlign="left">
+                        {props.title}
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+            </Heading>
+            <AccordionPanel pb={4}>{props.children}</AccordionPanel>
+        </AccordionItem>
+    );
+}
 
 export default function Concept() {
     return (
@@ -10,27 +36,24 @@ export default function Concept() {
                 <NormalLogo />
                 <Box h={100}></Box>
                 <SimpleBox title="Concept">
-                    <Heading fontSize="md" mt="2">
-                        NExus Overとは
-                    </Heading>
-                    <Box fontSize="sm" ml="5">
-                        <Text>
-                            WISHには多くの身内イベントがあります。五月祭、2イベ、夏合宿、駒場祭、12公、卒公・・・
-                            。そのどれにも当てはまらない全く新しいイベントが今年始まります。
-                        </Text>
-                        <Text>
+                    <Accordion defaultIndex={[0]} allowMultiple>
+                        <ConceptItem title="NExus Overとは">
+                            WISHには多くの身内イベントがあります。五月祭、2イベ、夏合宿、駒場祭、12公、卒公・・・。
+                            そのどれにも当てはまらない全く新しいイベントが今年始まります。
+                        </ConceptItem>
+                        <ConceptItem title="名前に込めた想い①">
                             「Nexus」とは「つながり」です。WISHは45年という長い歴史を持つサークルで、かつ上下のつながりが強いという特色を持っています。
                             代を超えたつながりはこのイベントのポイントとなります。
-                        </Text>
-                        <Text>
+                        </ConceptItem>
+                        <ConceptItem title="名前に込めた想い②">
                             もう一つの鍵は、ストリートダンスシーンとのつながりです。
                             キャストを見ていただければわかる通り、身内イベントとは思えない豪華さ、本気度です。
                             敷居の低い身内イベントで、ストリートダンスシーンの空気感を存分に楽しんでいただければ幸いです。
-                        </Text>
-                        <Text>
+                        </ConceptItem>
+                        <ConceptItem title="さいごに">
                             そしてこのイベントが、サークルという枠組みを超えた一歩を踏み出すきっかけになることを願っています。
-                        </Text>
-                    </Box>
+                        </ConceptItem>
+                    </Accordion>
                 </SimpleBox>
                 <SimpleBox title="Message">
                     <Image src="kakato.jpg" alt="kakato" />
@@ -56,7 +79,7 @@ export default function Concept() {
                     <Box fontSize="sm" ml="5">
                         <Text>
                             現役時代はほぼサークルの中で生きてきました。
-                            理由は自分でもわかりませんが、食わず嫌いというか、出不精というか、そんなぼんやりとしたものだった気がします。
+                            食わず嫌いというか、出不精というか、そんなぼんやりとした理由だった気がします。
                             引退して外のつながりも作り始めて、それもまた刺激的で楽しいと気づきました。
                         </Text>
                         <Text>
@@ -65,13 +88,11 @@ export default function Concept() {
                         </Text>
                         <Text>
                             その理由を大切にしながら、さらに良い刺激を得られるように、このイベントを開催します。
-                        </Text>
-                        <Text>
                             このイベントが、みんなの熱量を増やす燃料になってほしいと思っています。
                         </Text>
                     </Box>
                 </SimpleBox>
-                <Box h={100}></Box>
+                <Box h={50}></Box>
             </Box>
             <BackLinkBox />
         </>
