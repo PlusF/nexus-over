7d521b7949a1b8103f4db4e3722a566d78cf8c55
PlusF <rkaneda896@gmail.com>
2024-02-25 11:00:04 +0900

    cast coming soon

--- pages/casts.tsx
+++ pages/casts.tsx
@@ -17,35 +17,50 @@ function CastBox(props: { name: string; represents: string; img?: string }) {
 }
 
 export default function Casts() {
+    const comingSoon = true;
     return (
         <>
             <Box>
                 <NormalLogo />
                 <Heading h={100}></Heading>
-                <SimpleMotionBox title="Judges">
-                    <CastBox
-                        name="U-ki the retro"
-                        represents="Bixbite/GRAYSOURCE"
-                        img={'U-ki_the_retro.jpg'}
-                    />
-                    <CastBox name="RiN." represents="Novel Nextus/fidi" />
-                </SimpleMotionBox>
-                <SimpleMotionBox title="DJ">
-                    <CastBox name="OnokUro" represents="Mnchr-m" img={'OnokUro.jpg'} />
-                </SimpleMotionBox>
-                <SimpleMotionBox title="MC">
-                    <CastBox name="Kakato" represents="44th" img={'kakato.jpg'} />
-                </SimpleMotionBox>
-                <SimpleMotionBox title="Guest Battlers">
-                    <CastBox name="Tait Angle" represents="35th/LDC" img={'Tait_Angle.jpg'} />
-                    <CastBox
-                        name="軍犬ZERO"
-                        represents="39th/Mighty Beat Bation/Free Front"
-                        img={'軍犬ZERO.JPG'}
-                    />
-                    <CastBox name="Jr." represents="41st/Freaky Dickies" img={'Jr.jpg'} />
-                </SimpleMotionBox>
-                <Box h={50}></Box>
+                {comingSoon && (
+                    <>
+                        <SimpleMotionBox title="Casts">Coming soon...</SimpleMotionBox>
+                        <Box h={500}></Box>
+                    </>
+                )}
+                {!comingSoon && (
+                    <>
+                        <SimpleMotionBox title="Judges">
+                            <CastBox
+                                name="U-ki the retro"
+                                represents="Bixbite/GRAYSOURCE"
+                                img={'U-ki_the_retro.jpg'}
+                            />
+                            <CastBox name="RiN." represents="Novel Nextus/fidi" />
+                        </SimpleMotionBox>
+                        <SimpleMotionBox title="DJ">
+                            <CastBox name="OnokUro" represents="Mnchr-m" img={'OnokUro.jpg'} />
+                        </SimpleMotionBox>
+                        <SimpleMotionBox title="MC">
+                            <CastBox name="Kakato" represents="44th" img={'kakato.jpg'} />
+                        </SimpleMotionBox>
+                        <SimpleMotionBox title="Guest Battlers">
+                            <CastBox
+                                name="Tait Angle"
+                                represents="35th/LDC"
+                                img={'Tait_Angle.jpg'}
+                            />
+                            <CastBox
+                                name="軍犬ZERO"
+                                represents="39th/Mighty Beat Bation/Free Front"
+                                img={'軍犬ZERO.JPG'}
+                            />
+                            <CastBox name="Jr." represents="41st/Freaky Dickies" img={'Jr.jpg'} />
+                        </SimpleMotionBox>
+                        <Box h={50}></Box>
+                    </>
+                )}
             </Box>
             <BackLinkBox />
         </>
