3df966173c3143c7e752e8e59be396741ad909fb
PlusF <rkaneda896@gmail.com>
2024-02-29 12:14:17 +0900

    fix motion bug

--- components/motion.tsx
+++ components/motion.tsx
@@ -38,7 +38,7 @@ function ScrollOnce(props: { children: React.ReactNode; enableTap?: boolean }) {
         <motion.div
             initial="offscreen"
             whileInView="onscreen"
-            viewport={{ once: true, amount: 0.4 }}
+            viewport={{ once: true, amount: 0.2 }}
             style={{ width: '100%', height: '100%' }}
             whileTap={props.enableTap ? 'tap' : undefined}
             whileHover={props.enableTap ? 'hover' : undefined}
